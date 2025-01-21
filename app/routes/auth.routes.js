e826686c73a42703edb17a59d1a812c5ebc671c3
Murali <murali15062003@gmail.com>
2025-01-21 12:52:27 +0530

    Updating the Authentication routes

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/auth.controller");
-
+const {
+  checkEmailExists,
+  checkPhNoExists,
+} = require("../middlewares/checkEmailPassword");
 // Routes for the login and the register.
-router.post("/register", register);
+router.post("/register", checkEmailExists, checkPhNoExists, register);
 router.post("/login", login);
 
 module.exports = router;
