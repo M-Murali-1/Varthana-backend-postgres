e394255bb52304a567c4f7df8116bd27d7002f2a
Murali <murali15062003@gmail.com>
2025-01-21 12:52:54 +0530

    Updating the Employee routes

--- app/routes/employee.routes.js
+++ app/routes/employee.routes.js
@@ -1,15 +1,34 @@
 const { verifyToken } = require("../middlewares/authJwt");
+const {
+  checkEmailExists,
+  checkPhNoExists,
+} = require("../middlewares/checkEmailPassword");
 const express = require("express");
 const router = express.Router();
 const {
   findAll,
   updateOne,
   deleteOne,
+  createOne,
+  findOne,
 } = require("../controllers/employee.controller");
 
 // Routes for porforming the CRUD Operations oon the Employees.
 router.get("/getall", verifyToken, findAll);
-router.patch("/update/:id",verifyToken, updateOne);
-router.delete("/delete/:id",verifyToken, deleteOne);
-
+router.patch(
+  "/update/:id",
+  verifyToken,
+  checkEmailExists,
+  checkPhNoExists,
+  updateOne
+);
+router.delete("/delete/:id", verifyToken, deleteOne);
+router.post(
+  "/create",
+  verifyToken,
+  checkEmailExists,
+  checkPhNoExists,
+  createOne
+);
+router.post("/findone", findOne);
 module.exports = router;
